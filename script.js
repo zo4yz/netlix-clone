f20d099097b46a6bd3a28743859d4c65d09e07fe
zo4yz <140940249+zo4yz@users.noreply.github.com>
2025-01-05 18:47:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const nav = document.getElementById('nav')
+window.addEventListener("scroll", () => {
+if(window.scrollY>=100){
+    nav.classList.add("nav-black")
+} else {
+    nav.classList.remove("nav-black")
+}
+})
